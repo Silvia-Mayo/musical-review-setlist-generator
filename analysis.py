3808ee48b41e5a8f2d08d3059198137d54630a54
Silvia-Mayo <141570816+Silvia-Mayo@users.noreply.github.com>
2023-11-15 14:24:33 -0500

    Update analysis.py
    Added input and return descriptions, and actual return statements for methods returning nothing

--- analysis.py
+++ analysis.py
@@ -1,5 +1,5 @@
 ''' 
-Sophia Morgan and Sylvia Mayo
+Sophia Morgan and Silvia Mayo
 COMP 333
 analysis.py
 A file to actually run the analysis for the generator.
@@ -11,37 +11,72 @@ class Analysis():
        
     
     def __init__(self):
-        ''' A function to intialize the class analysis.'''
+        ''' A function to intialize the class analysis.
         
-    def sort_songs(self,key):
-        ''' A function to sort the songs based on user input'''
+            input: nothing
+            returns: nothing'''
+        return
         
-    def sort_singers(self,key):
-        ''' A function to sort the songs based on user input'''
+    def sort_songs(self, key):
+        ''' A function to sort the songs based on user input (in place)
+        
+            input: key for the song sorting
+            returns: nothing'''
+        return
+        
+    def sort_singers(self, key):
+        ''' A function to sort the singers based on user input (in place)
+        
+            input: key for the singer sorting
+            returns: nothing'''
+        return
         
     def add_song(self, song):
-        ''' A function to add a song to the database.'''
+        ''' A function to add a song to the database.
+            input: song to be added
+            returns: nothing'''
+        return
         
-    def add_singer(self, song):
-        ''' A function to add a singer profile to the database.'''
+    def add_singer(self, singer):
+        ''' A function to add a singer profile to the database.
         
-    def search_by_singer_profile(self,singer_profile):
+            input: singer to be added
+            returns: nothing'''
+        return
+        
+    def search_by_singer_profile(self, singer):
         '''A function that takes in a singer profile and returns a
-           list of songs that are suited for that singer'''
+           list of songs that are suited for that singer
            
-    def search_by_user_prefs(self, user_preds):
+           input: singer to be analyzed
+           returns: song list'''
+           
+    def search_by_user_prefs(self, user_prefs):
         ''' A function that takes in user preferences and returns a 
-            list of songs that are suited for that person'''
+            list of songs that are suited for that person
+            
+            input: user_prefs to be analyzed
+            returns: song list'''
             
     def search_by_group_number(self, number):
         ''' A function that searches for songs that are suited for a 
-            specific number of people.'''
+            specific number of people.
+            
+            input: number of people, int
+            returns: song list'''
             
     def songs_for_show(self, show):
         ''' A function that takes in a show and its length and other
             attributes if necessary and returns a list of songs that
-            will fill the allotted time.'''
+            will fill the allotted time.
+            
+            input: show length
+            returns: song list'''
         
     def get_user_input(self):
         ''' A function to get user input on what they are
-            aiming to find by using this generator'''
+            aiming to find by using this generator
+            
+            input: nothing
+            returns: nothing'''
+        return
