07b652c0b08580f36bbcb277abe99f38a8a3cd4b
sophmorg <shmorgan@wesleyan.edu>
2023-11-15 01:55:17 -0500

    added a few functions to search for either songs, singers, or user preferences.

--- analysis.py
+++ analysis.py
@@ -4,3 +4,44 @@ COMP 333
 analysis.py
 A file to actually run the analysis for the generator.
 '''
+
+class Analysis():
+    '''The generator will run analysis on the user inputted information
+       and will return a setlist.'''
+       
+    
+    def __init__(self):
+        ''' A function to intialize the class analysis.'''
+        
+    def sort_songs(self,key):
+        ''' A function to sort the songs based on user input'''
+        
+    def sort_singers(self,key):
+        ''' A function to sort the songs based on user input'''
+        
+    def add_song(self, song):
+        ''' A function to add a song to the database.'''
+        
+    def add_singer(self, song):
+        ''' A function to add a singer profile to the database.'''
+        
+    def search_by_singer_profile(self,singer_profile):
+        '''A function that takes in a singer profile and returns a
+           list of songs that are suited for that singer'''
+           
+    def search_by_user_prefs(self, user_preds):
+        ''' A function that takes in user preferences and returns a 
+            list of songs that are suited for that person'''
+            
+    def search_by_group_number(self, number):
+        ''' A function that searches for songs that are suited for a 
+            specific number of people.'''
+            
+    def songs_for_show(self, show):
+        ''' A function that takes in a show and its length and other
+            attributes if necessary and returns a list of songs that
+            will fill the allotted time.'''
+        
+    def get_user_input(self):
+        ''' A function to get user input on what they are
+            aiming to find by using this generator'''
