e0515faa9158ed100ccea333929076a28d936c68
sophmorg <shmorgan@wesleyan.edu>
2023-11-15 01:42:50 -0500

    added the function to run the generator.

--- user_interface.py
+++ user_interface.py
@@ -1,1 +1,30 @@
-''''''
+from data_objects import *
+
+class generator():
+    ''' A class generator to display all information to the use and allow
+        the user to take action.'''
+        
+    def __init__(self):
+        ''' Initialize the generator
+        '''
+        
+    def display_all_songs(self):
+        ''' A function to display all the songs that the generator has
+            in its database'''
+            
+    def add_song(self):
+        ''' A function to add a song to the database.'''
+    
+    def add_singer_profile(self):
+        ''' A function to add a singer profile to the database.'''
+        
+    def user_search(self,key):
+        ''' A function to allow the user to search for any song or singer
+            profile in the database.'''
+            
+    def run_generator(self):
+        ''' A function to run the generator based on user input. 
+            The goal of this is to look like a survey in which the
+            user is asked a few questions on what they are trying to 
+            achieve and then the generator will give them what they are
+            looking for.'''
