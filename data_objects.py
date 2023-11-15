4c7f7c5f88cd022a9c786dd5f275402f7de74580
sophmorg <shmorgan@wesleyan.edu>
2023-11-15 01:33:09 -0500

    made file data objects and added skeleton code.

---
+++ data_objects.py
@@ -0,0 +1,39 @@
+'''
+Sophia Morgan and Sylvia Mayo
+COMP 333
+Feature 1: Module that analyzes inputted singer profiles and determines appropriate songs
+'''
+
+class singer_profile():
+    ''' Class singer_profile contains the information for each singer profile 
+    using the generator'''
+    def __init__(self, range = '', style = '', genre = ''):
+        self.range = range
+        self.style = style
+        self.genre = genre
+        
+    def update_singer_profile(self):
+        ''' A function to allow the user to make alterations to the singer profile
+            that they are searching for.'''
+            
+    def display_singer_profile():
+        '''A function to display a specific singer profile.'''
+        
+        
+class song_profile():
+    ''' Class singer_profile contains the information for each singer profile 
+    using the generator'''
+    def __init__(self, range = '', style = '', genre = '', number = 0):
+        self.range = range
+        self.style = style
+        self.genre = genre
+        self.number = 0
+    
+    def song_to_singer(self, key):
+        ''' Compares a singer profile to the song profiles and if the 
+            attributes match, then this song is a match and will be 
+            returned to the user.'''
+            
+    def display_song_profile():
+        '''A function to display a specific song profile.'''
+    
