3423008b5954dd5e9a2fbbb1f46d77b655494b24
Silvia-Mayo <141570816+Silvia-Mayo@users.noreply.github.com>
2023-11-15 22:43:39 -0500

    Update user_interface.py
    Added input and return descriptions, and increased inputs for user-search

--- user_interface.py
+++ user_interface.py
@@ -1,5 +1,5 @@
 '''
-Sophia Morgan and Sylvia Mayo
+Sophia Morgan and Silvia Mayo
 COMP 333
 user_interface.py
 '''
@@ -12,25 +12,50 @@ class generator():
         
     def __init__(self):
         ''' Initialize the generator
+
+            input: nothing
+            returns: nothing
         '''
         
     def display_all_songs(self):
         ''' A function to display all the songs that the generator has
-            in its database'''
+            in its database
+
+            input: nothing
+            returns: nothing
+        '''
+        return
             
     def add_song(self):
-        ''' A function to add a song to the database.'''
+        ''' A function to add a song to the database.
+        
+            input: nothing
+            returns: nothing
+        '''
+        return
     
     def add_singer_profile(self):
-        ''' A function to add a singer profile to the database.'''
+        ''' A function to add a singer profile to the database.
         
-    def user_search(self,key):
+            input: nothing
+            returns: nothing
+        '''
+        
+    def user_search(self, keyword, kind = 'both', name = '', range = ''):
         ''' A function to allow the user to search for any song or singer
-            profile in the database.'''
+            profile in the database.
+
+            input: keyword (to search all fields), kind (either 'song', 'singer', or both), name, range
+            return:
+        '''
             
     def run_generator(self):
         ''' A function to run the generator based on user input. 
             The goal of this is to look like a survey in which the
             user is asked a few questions on what they are trying to 
             achieve and then the generator will give them what they are
-            looking for.'''
+            looking for.
+        
+            input: nothing
+            returns: nothing
+        '''
