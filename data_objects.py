393e165b7cc3eb22dbce67f695d7ad9e27a6f423
Silvia-Mayo <141570816+Silvia-Mayo@users.noreply.github.com>
2023-12-06 14:09:38 -0500

    Update data_objects.py
    Added note_range class and methods and modified some of the singer and song attributes

--- data_objects.py
+++ data_objects.py
@@ -6,12 +6,65 @@ data_objects.py
 import csv
 import numpy as np
 
+
+class note_range():
+    '''Class note_range allows the program to interpret a string
+    representing a range of musical notes numerically and meaningfully'''
+    def __init__(self, low, high):
+        self.low = low
+        self.high = high
+
+    def get_num(note):
+        '''Takes a string of a musical note and its octave and matches it to an integer
+        
+            input: a string of a note
+            returns: an integer representing the note'''
+        scale = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'G#', 'A', 'Bb', 'B']
+        octave = int(note[-1])
+        letter = note[:-1]
+        try:
+            scale_no = scale.index(letter)
+        except:
+            if letter == 'B#':
+                scale_no = 0
+                octave += 1
+            elif letter == 'Db':
+                scale_no = 1
+            elif letter == 'D#':
+                scale_no = 3
+            elif letter == 'Fb':
+                scale_no = 4
+            elif letter == 'E#':
+                scale_no = 5
+            elif letter == 'Gb':
+                scale_no = 6
+            elif letter == 'Ab':
+                scale_no = 8
+            elif letter == 'A#':
+                scale_no = 10
+            elif letter == 'Cb':
+                scale_no = 11
+                octave -= 1
+        num = scale_no + 12 * octave
+        return num
+
+    def contains_range(self, song_range):
+        '''Takes in a note_range object to compare to self and determine if a singer can sing a song
+        
+            input: note_range object of a song (self is presumably a singer)
+            output: boolean'''
+        if note_range.get_num(self.low) <= note_range.get_num(song_range.low):
+            if note_range.get_num(self.high) >= note_range.get_num(song_range.high):
+                return True
+        return False
+
 class singer():
     '''Class singer contains the information for each singer profile using the generator'''
-    def __init__(self, name = '', voice_range = '', voice_type = ''):
+    def __init__(self, name = '', voice_range = ''):
         self.name = name
-        self.range = voice_range
-        self.voice_type = voice_type
+        self.lohi = lohi
+        [low, high] = lohi.split('-')
+        self.voice_range = note_range(low, high)
         
     def update_singer_profile(self):
         ''' A function to allow the user to make alterations to the singer profile
@@ -26,15 +79,19 @@ class singer():
         
             input: nothing
             returns: nothing'''
+        print('       Name:', self.name)
+        print('Vocal Range:', self.lohi)
         return
         
         
 class song():
     '''Class song contains the information for each song using the generator'''
-    def __init__(self, name = '', length = 0, voice_range = '', genre = '', show_title = ''):
+    def __init__(self, name = '', length = 0, lohi = '', genre = '', show_title = ''):
         self.name = name
         self.length = length
-        self.range = voice_range
+        self.lohi = lohi
+        [low, high] = lohi.split('-')
+        self.note_range = note_range(low, high)
         self.genre = genre
         self.show_title = show_title
         
@@ -52,23 +109,24 @@ class song():
             return(csv_array)
         
     
-    def is_suitable(self, singer):
+    def is_suitable(self, one_singer):
         ''' Compares a singer profile to the song profiles and if the 
             attributes match, then this song is a match and will be 
             returned to the user.
             
             input: singer
             returns: Boolean'''
+        return one_singer.voice_range.contains_range(self.note_range)
             
     def display_song_profile():
         ''' A function to display a specific song profile.
         
             input: nothing
             returns: nothing'''
-        print(song.self.name)
-        print(song.self.length)
-        print(song.self.range)
-        print(song.self.genre)
-        print(song.self.show_title)
+        print('  Name:', self.name)
+        print('Length:', self.length, 'seconds (' + str(self.length // 60) + 'min', str(self.length % 60) + 's)')
+        print(' Range:', self.lohi)
+        print(' Genre:', self.genre)
+        print('  Show:', self.show_title)
         return
     
